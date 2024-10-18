6272e6ac1cc8ee7cac64e3b9233dd5bdefb44ffb
Davood <davood@bairuha.com>
2024-10-18 17:46:15 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,3 +11,24 @@ function App() {
 }
 
 export default App;
+
+
+
+// ===========routes ====================
+
+
+
+
+{/* <Routes>
+        <Route element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/search/:cat/:query/:date" element={<Search />} />
+        <Route path="/details/:id" element={<Auctiondetails />} />
+        <Route path="/listingAuctions/:id" element={<ItemListing />} />
+        <Route path="/listingAuctions" element={<ItemListing />} />
+        <Route path="/contactus" element={<Contactus />} />
+        <Route path="/changePassword" element={<ChangPassword />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/help" element={<Help />} /> */}
